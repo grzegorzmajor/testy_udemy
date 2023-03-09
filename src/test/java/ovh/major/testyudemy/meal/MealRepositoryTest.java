a425ed2974aaf118490e32ea069d3fcdd10908d6
grzegorzmajor <major.grzegorz@gmail.com>
2023-03-09 19:50:35 +0100

    final homework commit 7 added parametrized test method - for learning purposes only :)

--- src/test/java/ovh/major/testyudemy/meal/MealRepositoryTest.java
+++ src/test/java/ovh/major/testyudemy/meal/MealRepositoryTest.java
@@ -2,11 +2,14 @@ package ovh.major.testyudemy.meal;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 
 public class MealRepositoryTest {
@@ -130,6 +133,7 @@ public class MealRepositoryTest {
         assertThat(result.size(), is(1));
 
     }
+
     @Test
     void shouldBeAbleToFindMealByExactPriceTwoArgumentMethod() {
 
@@ -147,6 +151,35 @@ public class MealRepositoryTest {
         //then
         assertThat(result.size(), is(1));
 
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "10, 'Pizza', EXACT, 8, 0",
+            "16, 'Pizza XXL', EXACT, 8, 0",
+            "8, 'Hot Dog', EXACT, 8, 1",
+            "10, 'Pizza', LOWER, 11, 1",
+            "16, 'Pizza XXL', LOWER, 11, 0",
+            "8, 'Hot Dog', LOWER, 11, 1",
+            "10, 'Pizza', HIGHER, 8, 1",
+            "16, 'Pizza XXL', HIGHER, 8, 1",
+            "8, 'Hot Dog', HIGHER, 8, 0",
+
+    })
+    void shouldBeReturnedListWithMealIfItMatchesTheCondition(int price, String name, PriceCondition priceCondition, int searchPrice, int resultSize) {
+
+        //given
+        Meal meal = new Meal(price, name);
+        mealRepository.add(meal);
+
+        //when
+        List<Meal> result = mealRepository.findByPrice(searchPrice, priceCondition);
+
+        //then
+        assertAll(
+                () -> assertThat(result.size(), is(resultSize)),
+                () -> assertThat(result, resultSize == 1 ? contains(meal) : not(contains(meal)))
+        );
     }
 
 }
